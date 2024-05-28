cf33fa5c025fa74ac62c52e99a2fe40fd751d410
nataliaaraujoyazo <natalia.araujo@yazo.com.br>
2024-05-27 23:53:56 -0300

    chore: value concatenation

---
+++ concatenacao.py
@@ -0,0 +1,5 @@
+nomeDojogo = input ("Digite o nome do jogo\n") #input
+anoDeLancamento = int(input("Informe o ano de lancamento\n"))
+
+
+print(f"nome do jogo: {nomeDojogo} e o ano é {anoDeLancamento}")  #output
