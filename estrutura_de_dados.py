0d5dfa6dd99c623484623854ed73a62009443656
nataliaaraujoyazo <natalia.araujo@yazo.com.br>
2024-05-27 23:36:52 -0300

    chore:data type inference

---
+++ estrutura_de_dados.py
@@ -0,0 +1,7 @@
+#Python faz a inferencia de tipos.
+nomeDojogo = "God of War" #string
+anoDeLancamento = 2024 #int
+preco = 300.00 #float
+inclusoPlano = False #boolean
+
+print(type(anoDeLancamento))
