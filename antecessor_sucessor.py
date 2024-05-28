05cc28c2c15e77942aa6979dda2f99a5efe8ed9b
nataliaaraujoyazo <natalia.araujo@yazo.com.br>
2024-05-28 00:23:53 -0300

    chore: successor and predecessor

---
+++ antecessor_sucessor.py
@@ -0,0 +1,12 @@
+'''
+Escreva um programa em Python que leia um número e represente
+o número antecessor e sucessor desse número que foi lido,
+utilizando operadores de atribuição.
+'''
+
+numDigitado = int(input("Digite um numero \n"))
+
+antecessor = numDigitado +-1
+sucessor = numDigitado ++1
+
+print(f"## Antecessor de {numDigitado} é {antecessor} e seu Sucessor é {sucessor} ##")
