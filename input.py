be200713e9dfd9be8eae29346237daa6cefb6e08
nataliaaraujoyazo <natalia.araujo@yazo.com.br>
2024-05-27 23:46:26 -0300

    chore:reading data from input

---
+++ input.py
@@ -0,0 +1,7 @@
+nomeDojogo = input ("Digite o nome do jogo\n") #input
+anoDeLancamento = int(input("Informe o ano de lancamento\n"))
+#todo valor que vem do input é string, precisa ser convertido
+
+
+print('nome do jogo:',nomeDojogo)  #output
+print('ano do jogo:',anoDeLancamento)  #output
