dbcde4a884fda70efec236505279566e4d138f8e
nataliaaraujoyazo <natalia.araujo@yazo.com.br>
2024-05-30 20:43:06 -0300

    set

---
+++ set.py
@@ -0,0 +1,9 @@
+'''
+set {} :
+ - Não ordenada, mutável, elementos não duplicados
+ - METÓDOS : add(), remove(), union(), intersection(), difference()
+'''
+#objeto
+objeto = {1,2,3,4}
+objeto.add(4) #add elemento, será ignorado pois ja existe um n 4
+print("4 ignorado",objeto)
