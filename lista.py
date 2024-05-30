b0cfe8bc6503571be094f16aeaf42407c4143bec
nataliaaraujoyazo <natalia.araujo@yazo.com.br>
2024-05-29 22:28:43 -0300

    chore:list

---
+++ lista.py
@@ -0,0 +1,13 @@
+arrayDeJogos = ["mario", "god of war", "sonic","naruto","moral kombat"]
+
+# buscar os 2 primeiros itens
+print(arrayDeJogos[:2])
+
+# buscar o ultimo item
+print(arrayDeJogos[-1])
+
+# buscar jogos ate a posicao "sonic"
+print( arrayDeJogos[:3])
+
+# buscar jogos de "god of war" ate "naturo"
+print(arrayDeJogos[1:4])
