cfc0ab7e2c1f621148fe859baa4da541916a1bcc
nataliaaraujoyazo <natalia.araujo@yazo.com.br>
2024-06-04 02:05:47 -0300

    chore: function lambda

---
+++ functions/funcao_lambda_anonima.py
@@ -0,0 +1,16 @@
+def power(num): return num ** 2
+
+
+def pair(x): return x % 2 == 0
+def divNum(x, y): return x/y
+def reverse(s): return s[::-1]
+
+
+print(power(5))
+print(power(9))
+print(pair(27))
+print(pair(30))
+print(divNum(10, 2))
+print(divNum(7, 2))
+print(reverse("Função"))
+print(reverse("Tecnologia"))
